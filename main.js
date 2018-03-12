e045456f2d50a06b26bac82abbeccba998939a0d
MixedTea <nxtyofficially@gmail.com>
2018-03-12 00:01:39 -0600

    finally fixed the bug lmfao

--- main.js
+++ main.js
@@ -51,6 +51,7 @@ bot.on('message', function (msg) {
             https.get('https://api.mojang.com/users/profiles/minecraft/' + args[1], res => {
                 res.setEncoding("utf8");
                 let body = "";
+                let err = false;
                 res.on('data', data => {
                     body += data;
                 });
@@ -58,34 +59,42 @@ bot.on('message', function (msg) {
                     msg.channel.send("Invalid Username! Try checking your spelling.");
                 });
                 res.on('end', () => {
-                    body = JSON.parse(body);
-                    console.log(body.id);
-                    //https://api.mojang.com/user/profiles/<uuid>/names
-                    https.get('https://api.mojang.com/user/profiles/' + body.id + '/names', resp => {
-                        let names = "";
-                        resp.on('data', data => {
-                            names += data;
-                        });
-                        resp.on('end', () => {
-                            var ePlayer = new Discord.RichEmbed()
-                                .setTitle("Information on player " + body.name)
-                                .setThumbnail("https://visage.surgeplay.com/full/" + body.id)
-                                .addField("Hypixel stats: ", "https://plancke.io/hypixel/player/stats/" + body.name)
-                                .addField("NameMC Stats: ", "https://namemc.com/name/" + body.name);
-                            msg.channel.send(ePlayer);
-                            names = JSON.parse(names);
-                            var eNames = new Discord.RichEmbed()
-                                .setDescription(body.name + "'s name history:");
-                            for (var i = 0; i < names.length; i++) {
-                                eNames.addField("Name #" + (i + 1), names[i].name);
-                            }
-                            msg.channel.send(eNames);
-                            resp.on("error", () => {
-                                msg.channel.send("Invalid Username! Try checking your spelling.");
-                            });
+                    try {
+                        body = JSON.parse(body);
+                    } catch (e) {
+                        msg.channel.send("Invalid Username!");
+                        err = true;
+                    }
+                    if (!err) {
+                        console.log(body.id);
+                        //https://api.mojang.com/user/profiles/<uuid>/names
+                        https.get('https://api.mojang.com/user/profiles/' + body.id + '/names', resp => {
+                                let names = "";
+                                resp.on('data', data => {
+                                    names += data;
+                                });
+                                resp.on('end', () => {
+                                    var ePlayer = new Discord.RichEmbed()
+                                        .setTitle("Information on player " + body.name)
+                                        .setThumbnail("https://visage.surgeplay.com/full/" + body.id)
+                                        .addField("Hypixel stats: ", "https://plancke.io/hypixel/player/stats/" + body.name)
+                                        .addField("NameMC Stats: ", "https://namemc.com/name/" + body.name);
+                                    msg.channel.send(ePlayer);
+                                    names = JSON.parse(names);
+                                    var eNames = new Discord.RichEmbed()
+                                        .setDescription(body.name + "'s name history:");
+                                    for (var i = 0; i < names.length; i++) {
+                                        eNames.addField("Name #" + (i + 1), names[i].name);
+                                    }
+                                    msg.channel.send(eNames);
+                                    resp.on("error", () => {
+                                        msg.channel.send("Invalid Username! Try checking your spelling.");
+                                    });
 
+                                });
+                            
                         });
-                    });
+                    }
                 });
             });
             break;
@@ -94,24 +103,27 @@ bot.on('message', function (msg) {
             https.get('https://api.mojang.com/users/profiles/minecraft/' + args[1], res => {
                 res.setEncoding("utf8");
                 let body = '';
+                let err = false;
                 res.on('data', data => {
                     body += data;
                 });
                 res.on('end', () => {
-                   body = JSON.parse(body);
-                    var eSkin = new Discord.RichEmbed()
-                        .setTitle(body.name + "'s skin:")
-                        .setImage("https://visage.surgeplay.com/full/" + body.id)
-                        .setThumbnail("https://visage.surgeplay.com/skin/" + body.id)
-                        .setDescription("https://namemc.com/profile/" + body.name);
-                    msg.channel.send(eSkin);
+                    try {
+                        body = JSON.parse(body);
+                    } catch (e) {
+                        console.log(e);
+                        msg.channel.send("Invalid Username! Try checking your spelling!");
+                        err = true;
+                    }
+                    if (!err) {
+                        var eSkin = new Discord.RichEmbed()
+                            .setTitle(body.name + "'s skin:")
+                            .setImage("https://visage.surgeplay.com/full/" + body.id)
+                            .setThumbnail("https://visage.surgeplay.com/skin/" + body.id)
+                            .setDescription("https://namemc.com/profile/" + body.name);
+                        msg.channel.send(eSkin);
+                    }
                 });
-                res.on("error", () => {
-                    msg.channel.send("Invalid Username! Try checking your spelling.");
-                });
-            }).on("error", (e) => {
-                console.log(e);
-                msg.channel.send(e);
             });
             break;
         default:
